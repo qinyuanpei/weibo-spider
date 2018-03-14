b9d5ca5b15a4287892253ae28a58b5e2e6c9ae37
PayneQin <qinyuanpei@chinasofti.com>
2018-03-14 14:23:34 +0800

    Update drawing.py

---
+++ drawing.py
@@ -0,0 +1,30 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# 绘制柱形图
+def bar(title, data, fileName):
+    pass
+
+# 绘制饼图
+def pie(title, data, fileName):
+    labels = data.keys()
+    counts = data.values()
+    colors = ['red','yellowgreen','lightskyblue']
+    plt.figure(figsize=(8,5), dpi=80)
+    plt.axes(aspect=1) 
+    plt.pie(counts, #性别统计结果
+            labels=labels, #性别展示标签
+            colors=colors, #饼图区域配色
+            labeldistance = 1.1, #标签距离圆点距离
+            autopct = '%3.1f%%', #饼图区域文本格式
+            shadow = False, #饼图是否显示阴影
+            startangle = 90, #饼图起始角度
+            pctdistance = 0.6 #饼图区域文本距离圆点距离
+    )
+    plt.legend(loc='upper right',)
+    plt.title(title)
+    plt.show()
+
+# 绘制词云
+def wordcloud(text,background,fileName)：
+    pass
